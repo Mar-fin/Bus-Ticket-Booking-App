f18f7af63e0c93184a2e94dbe3ee1967e5b2fc1c
Mar-fin <v.marvin.t@gmail.com>
2022-04-04 12:40:31 +0530

    Models

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32
+    },
+    address: {
+      type: String,
+      maxlength: 32
+    },
+    phone: {
+      type: Number,
+      max: 9999999999,
+      required: true
+    },
+    dob: {
+        type: Date,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    bookings: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Booking'
+      }
+    ]
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
