64a348afc83ffdbb8bc67df4f8676f7d3c1140bb
Mar-fin <v.marvin.t@gmail.com>
2022-04-12 21:27:43 +0530

    Admin Model

--- models/Admin.js
+++ models/Admin.js
@@ -1,37 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const adminSchema = new mongoose.Schema(
-  {
-    companyName: {
-      type: String,
-      required: true,
-      maxlength: 32
-    },
-    officeAddress: {
-      type: String,
-      required: true,
-      maxlength: 64
-    },
-    phone: {
-      type: Number,
-      max: 9999999999,
-      required: true
-    },
-    email: {
-      type: String,
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    buses: [
-          {
-            type: Schema.Types.ObjectId,
-            ref: 'Bus'
-          }
-      ]
+const adminSchema = new mongoose.Schema({
+  companyName: {
+    type: String,
+    required: true,
+    maxlength: 32,
   },
-);
+  officeAddress: {
+    type: String,
+    required: true,
+    maxlength: 64,
+  },
+  phone: {
+    type: Number,
+    max: 9999999999,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  buses: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Bus',
+    },
+  ],
+});
 
-module.exports = mongoose.model("Admin", adminSchema);
+module.exports = mongoose.model('Admin', adminSchema);
